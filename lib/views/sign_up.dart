bb1fb68e778ca4bb1490b722e2a8401e80d7c048
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-14 19:03:33 +0300

    add column and text in sign up page

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -1,17 +1,17 @@
-// import 'package:flutter/material.dart';
-//
-// class SignUp extends StatelessWidget {
-//   const SignUp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Column(
-//       mainAxisAlignment: MainAxisAlignment.start,
-//       crossAxisAlignment: CrossAxisAlignment.center,
-//       children: [
-//         Text("Sign Up",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.black),)
-//       ],
-//
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("Sign Up",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.black),)
+      ],
+
+    );
+  }
+}
