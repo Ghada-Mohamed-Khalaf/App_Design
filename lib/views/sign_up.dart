ebd3c42b53f8f575084eee8a3896c523c80b731d
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-08-14 19:59:45 +0300

    add textField for email

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -7,15 +7,31 @@ class SignUp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(body:
        SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Sign Up",style: TextStyle(fontWeight: FontWeight.bold,fontSize:33,color: Colors.black),),
-          SizedBox(height: 5,),
-          Text("Enter your details below & free sign up",style: TextStyle(color: Colors.grey,fontSize: 20),),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Sign Up",style: TextStyle(fontWeight: FontWeight.bold,fontSize:33,color: Colors.black),),
+            SizedBox(height: 5,),
+            Text("Enter your details below & free sign up",style: TextStyle(color: Colors.grey,fontSize: 20),),
+            SizedBox(height: 50,),
+    Text("Your Email",style: TextStyle(fontSize: 17),),
+    TextField(decoration: InputDecoration(
 
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: BorderSide(color: Colors.orange,
+    ),
+    ),
+    ),
+    ),
+
+    ],
+
+
+        ),
       ),
        ),
     );
